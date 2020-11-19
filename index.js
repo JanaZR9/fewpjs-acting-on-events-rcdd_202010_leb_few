397c2f6c4751f3ebaf356b7cb17bca4a2eabfa66
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-19 20:01:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 
-function moveDodgerLeft() {
+function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
  
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
